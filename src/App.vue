459660991b1115111d7a9a04ba98757d21ed6b34
Odohi David <odohi.david@gmail.com>
2020-04-07 21:06:24 +0100

    create app.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="sans-serif">
+    <Header/>
+    <Home msg="Tasks Pointing Sessions" class="black-60"/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Footer,
+    Header
+  }
+}
+</script>
+
+<style>
+@import url(./assets/tachyons.css);
+</style>
