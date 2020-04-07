37e61c76513eb18720917562e99448d1cc116b31
Odohi David <odohi.david@gmail.com>
2020-04-07 21:07:31 +0100

    create home component

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="tc red">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
